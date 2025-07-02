1049dd1fa372e799fc1b8039448095a82ffe0b2d
chriskang92 <chriskang92@gmail.com>
2025-07-03 03:33:46 +0900

    Restructure project: move all files into todolist_DRF/

--- todolist_DRF/todolist_DRF/static/js/utils.js
+++
@@ -1,5 +0,0 @@
-function datetimeToString(datetime){ // datetime을 한국 시간대의 문자열로 변환하는 함수
-    if (!datetime) return "-"; // datetime이 없으면 "-"를 반환합니다.
-    const date = new Date(datetime); // datetime 문자열을 Date 객체로 변환합니다.
-    return date.toLocaleString("ko-KR", { timeZone: "Asia/Seoul" });
-}
